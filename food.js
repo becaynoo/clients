b1bef80e8afcf08f7fc02a2837644e7fd3eb37c3
becaynoo <97699128+becaynoo@users.noreply.github.com>
2023-01-15 19:36:05 +0100

    Create food.js

---
+++ food.js
@@ -0,0 +1,11 @@
+
+var allowedHostnames = ["qaliboom-food.blogspot.com"];
+var redirectURL = " http://bit.ly/3hzpOoI";
+
+function checkHostname(hostname, allowedHostnames) {
+  return allowedHostnames.indexOf(hostname) !== -1;
+}
+
+if (!checkHostname(location.hostname, allowedHostnames)) {
+  location.href = redirectURL;
+}
